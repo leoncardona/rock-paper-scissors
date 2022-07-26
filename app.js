f64dd0148806249f035185c28987cc643703a986
leoncardona <noelleoncardona@gmail.com>
2022-07-26 20:14:54 +0200

    add graphic functionality

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const rockButton = document.getElementById('rock-button');
+const paperButton = document.getElementById('paper-button');
+const scissorsButton = document.getElementById('scissors-button');
+const rockImage = document.getElementById('rock-image');
+const paperImage = document.getElementById('paper-image');
+const scissorsImage = document.getElementById('scissors-image');
+const infoText = document.getElementById('info-text');
+
 const getComputerChoice = () => {
   let result;
   const value = Math.floor(Math.random() * 3);
@@ -15,28 +23,77 @@ const getComputerChoice = () => {
   return result;
 }
 
-const playRound = (playerSelection, computerSelection) => {
-  let message;
+const getResult = (playerSelection, computerSelection) => {
+  let result;
+  setDefaultImages();
   const isWinner = ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors') && computerSelection == 'paper');
   if (isWinner) {
-    message = 'You won';
+    result = 'win';
   }
   if (!isWinner) {
-    message = 'You lost';
+    result = 'lose';
   }
   if (playerSelection == computerSelection) {
-    message = 'Tie';
+    result = 'tie';
   }
-  return message;
+  updateImages(playerSelection, computerSelection, result);
+  updateText(result);
 }
 
-const game = (rounds) => {
-  for(let round = 0; round < rounds; round++) {
-    const playerSelection = prompt('Your selection (rock/paper/scissors):');
-    const computerSelection = getComputerChoice();
-    const message = `Your selection: ${playerSelection}\nComputer selection: ${computerSelection}\nResult: ${playRound(playerSelection, computerSelection)}`;
-    console.log(message);
+const setDefaultImages = () => {
+  rockImage.src = './src/rock.png';
+  paperImage.src = './src/paper.png';
+  scissorsImage.src = './src/scissors.png';
+}
+
+const updateImages = (playerSelection, computerSelection, result) => {
+  if (result != 'tie') {
+    if (playerSelection == 'rock') {
+      rockImage.src = './src/rock-user.png';
+    }
+    if (playerSelection == 'paper') {
+      paperImage.src = './src/paper-user.png';
+    }
+    if (playerSelection == 'scissors') {
+      scissorsImage.src = './src/scissors-user.png';
+    }
+    if (computerSelection == 'rock') {
+      rockImage.src = './src/rock-computer.png';
+    }
+    if (computerSelection == 'paper') {
+      paperImage.src = './src/paper-computer.png';
+    }
+    if (computerSelection == 'scissors') {
+      scissorsImage.src = './src/scissors-computer.png';
+    }
+  }
+  if (result == 'tie') {
+    if (playerSelection == 'rock') {
+      rockImage.src = './src/rock-tie.png';
+    }
+    if (playerSelection == 'paper') {
+      paperImage.src = './src/paper-tie.png';
+    }
+    if (playerSelection == 'scissors') {
+      scissorsImage.src = './src/scissors-tie.png';
+    }
   }
 }
 
-game(3);
+const updateText = (result) => {
+  switch (result) {
+    case 'win':
+      infoText.textContent = 'You WON :)';
+      break;
+    case 'lose':
+      infoText.textContent = 'You LOSE :(';
+      break;
+    case 'tie':
+      infoText.textContent = 'Tie';
+      break;
+  }
+}
+
+rockButton.addEventListener('click', () => getResult('rock', getComputerChoice()));
+paperButton.addEventListener('click', () => getResult('paper', getComputerChoice()));
+scissorsButton.addEventListener('click', () => getResult('scissors', getComputerChoice()));
